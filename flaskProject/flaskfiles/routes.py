080995bc9aa6ab85b3bd014c46b3a12e8e06fc99
Luke D'Amato <luke.damato.20@um.edu.mt>
2022-05-16 14:20:46 +0200

    updated flaskfiles

--- flaskProject/flaskfiles/routes.py
+++ flaskProject/flaskfiles/routes.py
@@ -26,3 +26,15 @@ def demo_result():
     text_manipulation(text)
 
     return redirect('demo.html')
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    # note that we set the 404 status explicitly
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def page_not_found(e):
+    # note that we set the 404 status explicitly
+    return render_template('500.html'), 404
